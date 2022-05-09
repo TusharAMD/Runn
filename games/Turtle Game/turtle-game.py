810d0d7aadc1515fb29cefd6d701572bc09a8a61
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-08 23:18:17 -0700

    :memo: finish line created

--- games/Turtle Game/turtle-game.py
+++ games/Turtle Game/turtle-game.py
@@ -32,3 +32,20 @@ t.goto(-140,230)
 t.color('white')
 t.pendown()
 t.write('TURTLE RACE',font=('Arial',30,'bold'))
+
+#TO CREAT FINISH LINE
+def f_line(x,y,color):
+    t.speed(0)
+    gap_size = 20
+    t.penup()
+    t.goto(x,y)
+    t.shape('square')
+    t.color(color)
+    for i in range(10):
+        t.goto(x,(y-(i*gap_size*2)))
+        t.stamp()
+        
+f_line(250,170,'white')
+f_line(230,190,'white')
+f_line(230,170,'black')
+f_line(250,190,'black')
