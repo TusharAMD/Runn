8036d1bd6b3d430580edba371f90466aac21bfcb
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-16 08:47:34 -0700

    :memo: python pygame fun added

--- games/Ball Pool Game/start.py
+++ games/Ball Pool Game/start.py
@@ -1,3 +1,12 @@
 import pygame
 import sys
 from math import *
+
+pygame.init()
+width = 660
+height = 360
+outerHeight = 400
+margin = 30
+display = pygame.display.set_mode((width, outerHeight))
+pygame.display.set_caption("Ball Pool")
+clock = pygame.time.Clock()
