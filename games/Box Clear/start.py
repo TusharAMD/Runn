6f3efa3f20ef29c4e25206da765d19fb632c6404
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-17 20:47:00 -0700

    :memo: msg added for help

--- games/Box Clear/start.py
+++ games/Box Clear/start.py
@@ -7,3 +7,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import PhotoImage
 from tkinter import messagebox
+
+msg = 'Click a square, you get a number.\
+That number is the number of how many mines are surrounding it.\
+If you find the mine, you can open "unopened" squares around it, opening more areas.'
