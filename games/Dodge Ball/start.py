d4676e6636e072aa8ab8c8622112c95e7713be4a
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-18 22:17:59 -0700

    :memo: module imported

---
+++ games/Dodge Ball/start.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------------
+#
+# Dodge The Ball!
+# Language - Python
+# Modules - pygame, sys, random, math
+#
+# Controls - Mouse Movement
+#
+# Author - Rahul Mahato
+# -----------------------------------------------------------------------------
+
+
+import pygame
+import sys
+import random
+from math import *
